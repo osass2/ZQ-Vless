6beded414d37243c6f41b2efe0130d434770b0a6
osass2 <akunnuklir2@gmail.com>
2025-09-05 21:02:24 +0800

    Update vless.js

--- vless.js
+++ vless.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "2eb6210f-0edd-404b-a030-a7ccee30c2cc";
 
 const proxyIPs = ["ts.hpc.tw"];
 const cn_hostnames = [''];
